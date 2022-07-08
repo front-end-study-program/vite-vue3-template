c8085594aa0cc695c23e79f525c01acd6d448ad6
yanquanfahei <1449826851@qq.com>
2022-07-08 15:07:26 +0800

    chore: add stylelint

--- src/App.vue
+++ src/App.vue
@@ -5,17 +5,20 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <img
+    alt="Vue logo"
+    src="./assets/logo.png"
+  >
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
 
 <style>
 #app {
+  margin-top: 60px;
+  color: #2c3e50;
   font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
